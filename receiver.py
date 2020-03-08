309c0be4fb5a102b6913d4a0803ae3e8b65e4f7c
sebastien-lb <sebast.lubineau@laposte.net>
2020-03-08 21:23:51 +0100

    feat: receive array

--- receiver.py
+++ receiver.py
@@ -5,12 +5,16 @@ import websockets
 import requests
 import json
 
-API_ENDPOINT = "http://localhost:8080/transaction-validation"
+API_ENDPOINT_SOCRE = "http://localhost:8080/transaction-validation"
+API_WEBSOCKET_TRANSACTION = "ws://localhost:8080/transaction-stream/username/"
 TEAM_NAME="team1"
 TEAM_PASSWORD="pass1"
 
+def isTransactionFraudulent(transaction):
+    return True
+
 def send_value(transaction_id, is_fraudulent):
-    url = API_ENDPOINT + "?username=" + TEAM_NAME + "&password=" + TEAM_PASSWORD
+    url = API_ENDPOINT_SOCRE + "?username=" + TEAM_NAME + "&password=" + TEAM_PASSWORD
     # data to be sent to api
     data = {
         'fraudulent': is_fraudulent,
@@ -23,13 +27,18 @@ def send_value(transaction_id, is_fraudulent):
     requests.post(url = url, json = data, )
 
 async def receive_transaction():
-    uri = "ws://localhost:8080/transaction-stream/username/" + TEAM_NAME
+    uri = API_WEBSOCKET_TRANSACTION + TEAM_NAME
     async with websockets.connect(uri) as websocket:
         await websocket.send("Hello world!")
 
         while True:
+            # data recieved from the API
             received = json.loads(await websocket.recv())
-            # send_value(received['id'], False)
+            print(('data received', received))
+
+            # Sending data back to the API to compute score
+            for transaction in received:
+                send_value(transaction['id'], True)
 
 asyncio.get_event_loop().run_until_complete(receive_transaction())
 
